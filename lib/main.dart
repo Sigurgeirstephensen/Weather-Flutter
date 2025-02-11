dbf2a11d0f3fad3f8bd35a11829fb8fb5045ece4
piotra <piotr20@ru.is>
2025-02-11 19:09:54 +0000

    first cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/application/core/initialization_cubit.dart';
 import 'package:weather/presentation/overview_page.dart';
+import 'package:weather/setup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const OverviewPage(),
+    return BlocProvider(
+      create: (BuildContext context) => getIt<InitializationCubit>()..init(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const OverviewPage(),
+      ),
     );
   }
 }
